b835da9d7e5a9831a01a42ba925d1cede85c647c
Krypton83 <kmonkiew@wp.pl>
2024-11-17 21:19:47 +0100

    deleted keylogger

--- events/clientMessage.js
+++ events/clientMessage.js
@@ -4,33 +4,36 @@ export function clientMessage(wia, ws) {
     wia = JSON.parse(wia);
     let sn = gracze.get(ws);
 
-    if (sn.czy_pierwszy) {
+    if (sn.czy_pierwszy) 
+    {
         sn.nick = wia.nick;
         sn.czy_pierwszy = false;
         gracze.set(ws, sn);
         chat.push('<span style="color: green;">Gracz ' + sn.nick + ' dołączył do gry</span>');
     } 
-    else {
-        let klawisz = wia.klawisz;
+    else 
+    {
+        let ruch = wia.ruch;
         let wiadomosc = wia.wiadomosc;
         if (wiadomosc != undefined) {
             if (wiadomosc != null) {
                 chat.push(wiadomosc);
             }
         } 
-        else {
+        else 
+        {
             let waz = gracze.get(ws);
 
-            if ((klawisz == 'KeyD' || klawisz == "ArrowRight") && waz.dx >= 0) {
+            if (ruch == 'p' && waz.dx >= 0) {
                 waz.dx = grid;
                 waz.dy = 0;
-            } else if ((klawisz == 'KeyA' || klawisz == "ArrowLeft") && waz.dx <= 0) {
+            } else if (ruch == 'l' && waz.dx <= 0) {
                 waz.dx = -grid;
                 waz.dy = 0;
-            } else if ((klawisz == 'KeyW' || klawisz == "ArrowUp") && waz.dy <= 0) {
+            } else if (ruch == 'g' && waz.dy <= 0) {
                 waz.dy = -grid;
                 waz.dx = 0;
-            } else if ((klawisz == 'KeyS' || klawisz == "ArrowDown") && waz.dy >= 0) {
+            } else if (ruch == 'd' && waz.dy >= 0) {
                 waz.dy = grid;
                 waz.dx = 0;
             }
--- public/snake2.js
+++ public/snake2.js
@@ -8,7 +8,7 @@ let jablko;
 let wynik = 0;
 let gameover = false; //gdy jest rowny true - koniec gry
 let socket;
-let klawisz;
+let ruch;
 let i = 0;
 let napisy = [];
 let ipAddr;
@@ -114,15 +114,42 @@ function loop() {
 
     window.addEventListener('keydown', (e) => {
         //Obłsuga klawiszy
-        if (e.code != klawisz) {
+
+        let klawisz = e.code;
+        let nruch;
+
+        if (klawisz == 'KeyD' || klawisz == "ArrowRight") 
+        {
+            nruch = "p";
+        } 
+        else if (klawisz == 'KeyA' || klawisz == "ArrowLeft")
+        {
+            nruch = "l";
+        } 
+        else if (klawisz == 'KeyW' || klawisz == "ArrowUp") 
+        {
+            nruch = "g";
+        }
+        else if (klawisz == 'KeyS' || klawisz == "ArrowDown") 
+        {
+            nruch = "d";
+        }
+
+        if(nruch != ruch)
+        {
             socket.send(
                 JSON.stringify({
-                    klawisz: e.code,
+                    ruch: nruch,
                 }),
             );
-            if (wiadomosc) wiadomosc = undefined;
         }
-        klawisz = e.code;
+
+        ruch = nruch;
+
+        if (wiadomosc)
+        {
+            wiadomosc = undefined;
+        }
     });
     ranking.innerHTML = '';
     napisy.sort((a, b) => b.wynik - a.wynik);
