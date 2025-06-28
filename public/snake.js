0857abe3b7d3e183ddca89f45dba56a51829a5bb
Krypton83 <91143092+Krypton83@users.noreply.github.com>
2025-06-28 20:57:53 +0200

    responsiveness

--- public/snake.js
+++ public/snake.js
@@ -366,6 +366,7 @@ function joinToGame()
                 
                 snakeX = wiad.snakeX+(przesuniecie/16) - screen.width/34
                 snakeY = wiad.snakeY+(przesuniecie/16) - screen.height/34
+                }
 
                 if(wiad.zakonczenie_gry && !zakonczenie_gry)
                 {
